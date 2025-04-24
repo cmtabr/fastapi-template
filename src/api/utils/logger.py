0831b78886be42d596b519b3f40b3d6fb7bd66aa
Caio Martins <cmtabr@gmail.com>
2025-04-23 22:35:37 -0300

    feat: :sparkles: improve logger object implementation

--- src/api/utils/logger.py
+++ src/api/utils/logger.py
@@ -3,36 +3,24 @@ import logging
 
 class Logger:
     """
-    Logger is a class that sets up and provides a configured logger for logging messages.
-    
-    Methods:
-    --------
-    __init__(logger_name: str):
-        Initializes the Logger instance with a specified logger name.
-        
-    _logger_setup():
-        Sets up the logging handler and formatter.
+    Logger class to configure and provide a named logger instance.
+
+    Parameters:
+    -----------
+    logger_name : str
+        The name of the logger.
+    level : int, optional
+        Logging level (default is logging.INFO).
     """
-    def __init__(self, logger_name: str):
-        """
-        Initializes the Logger instance.
-        
-        Parameters:
-        -----------
-        logger_name : str
-            The name to be assigned to the logger instance.
-        """
+    def __init__(self, logger_name: str, level: int = logging.INFO):
         self.logger = logging.getLogger(name=logger_name)
-        self.logger.setLevel(logging.INFO)
-        self._logger_setup()
+        self.logger.setLevel(level)
+        if not self.logger.hasHandlers():
+            self._logger_setup()
 
     def _logger_setup(self):
         """
         Sets up the logging handler and formatter.
-        
-        This method configures the logger to use a StreamHandler with a specific
-        format for log messages, including the timestamp, logger name, log level,
-        and the message itself.
         """
         handler = logging.StreamHandler()
         formatter = logging.Formatter(
