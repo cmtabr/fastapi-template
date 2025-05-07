d7ab5d40667c02fb350f70f1b199f85ff320f7fc
Caio Martins <cmtabr@gmail.com>
2025-05-06 22:35:40 -0300

    feat: :sparkles: adds roles to user table model and uses explicit packages imports

--- src/api/models/users.py
+++ src/api/models/users.py
@@ -1,7 +1,10 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.types import DateTime
+from sqlalchemy.types import DateTime, Enum
+
 from datetime import datetime
+
+from api.enums import UserRolesEnum
 from .shared import Base, metadata
 
 class Users(Base):
@@ -12,9 +15,12 @@ class Users(Base):
     user_id: Mapped[str] = mapped_column(
         String(36), primary_key=True, unique=True, nullable=False
     )
-    username: Mapped[str] = mapped_column(String(16), nullable=True)
-    password_hash: Mapped[str]
-    email: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(String(16), nullable=True, unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    role: Mapped[UserRolesEnum] = mapped_column(
+        Enum(UserRolesEnum), default=UserRolesEnum.USER
+    )
     is_active: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[DateTime] = mapped_column(
         DateTime, default=datetime.now()
