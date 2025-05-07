9cdc9cd51c26b2400b86c9842056bcb193b97656
Caio Martins <cmtabr@gmail.com>
2025-05-06 23:01:49 -0300

    refactor: :recycle: improves token handler impl adding better exceptions

--- src/api/utils/token_handler.py
+++ src/api/utils/token_handler.py
@@ -2,38 +2,57 @@ from fastapi import Header, HTTPException
 from jwt import decode, encode
 
 from datetime import datetime, timedelta
-from typing import Annotated
 
-from config import AuthSettings
-from schemas import TokenHeaderSchema
+from api.config import AuthSettings
+from api.schemas import TokenSchema
 from .logger import Logger
 
 _logger = Logger(logger_name=__name__).logger
 
+_auth_settings = AuthSettings()  # Load settings once at the module level
 
-class TokenHandler(AuthSettings):
+
+class TokenHandler:
     def __init__(self):
         """
         TokenHandler is a class that handles JWT token creation and decoding
         """
-        super().__init__()
+        self.secret = _auth_settings.secret
+        self.algorithm = _auth_settings.algorithm
 
-    async def create_token(self, user_id: str) -> TokenHeaderSchema:
+    async def create_token(self, user_id: str) -> TokenSchema:
         try:
             payload = {
-                'user_id': user_id,
+                'sub': user_id,
                 'exp': datetime.now() + timedelta(seconds=300)
             }
             token = encode(payload, self.secret, algorithm=self.algorithm)
-            return TokenHeaderSchema(key="X-Key", value=token)
+            return TokenSchema(value=token)
         except Exception as e:
             _logger.error(f"Error creating token: {str(e)}")
-            raise
+            raise HTTPException(
+                status_code=500,
+                detail="Internal server error",
+                headers={"X-Error": "Token creation failed"}
+            )
 
-    async def decode_token(self, token: Annotated[TokenHeaderSchema, Header()]) -> dict:
+    async def decode_token(self, token: str) -> dict:
         try:
-            payload: dict = decode(token.value, self.secret, algorithms=self.algorithm)
+            _logger.info(f"Decoding token: {token}")
+            if not "Bearer" in token:
+                _logger.error("Invalid token format")
+                raise HTTPException(
+                    status_code=403,
+                    detail="Invalid token format",
+                    headers={"X-Error": "Token format is invalid"}
+                )
+            token = token.split(" ")[1]
+            payload = decode(token, self.secret, algorithms=self.algorithm)
             return payload
         except Exception as e:
             _logger.error(f"Error decoding token: {str(e)}")
-            raise
+            raise HTTPException(
+                status_code=403,
+                detail="Invalid token",
+                headers={"X-Error": "Token decoding failed"}
+            )
