f7853849d499900fc7edb7edae1cadb9cba76307
Caio Martins <cmtabr@gmail.com>
2025-05-13 22:37:57 -0300

    refactor: :recycle: improves session manager class abastraction

--- src/api/utils/session_manager.py
+++ src/api/utils/session_manager.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, Session, sessionmaker
+
 from api.config import DatabaseSettings
 from .logger import Logger
 
@@ -13,34 +14,39 @@ class SessionFactory:
     _engine = None
     _session_factory = None
 
-    def __init__(self):
-        if SessionFactory._engine == None:
-            SessionFactory._engine = create_engine(
+    @classmethod
+    def initialize_engine(cls) -> scoped_session:
+        if cls._engine is None:
+            cls._engine = create_engine(
                 url=_database_settings.connection_url,
                 pool_size=_database_settings.pool_size,
                 max_overflow=_database_settings.max_overflow,
                 pool_pre_ping=True
             )
-            SessionFactory._session_factory = scoped_session(
+            cls._session_factory = scoped_session(
                 sessionmaker(
-                    bind=SessionFactory._engine,
+                    bind=cls._engine,
                     expire_on_commit=False,
                     autoflush=False
                 )
             )
 
-    @property
-    def session_factory(self) -> Session:
-        return SessionFactory._session_factory
+    @classmethod
+    def get_session(cls) -> Session:
+        if cls._session_factory is None:
+            cls.initialize_engine()
+        return cls._session_factory
+
+    @classmethod
+    def remove_session(cls) -> None:
+        if cls._session_factory:
+            cls._session_factory.remove()
 
 
 class SessionManager:
     """Thread-safe SQLAlchemy session manager using context protocol."""
-    def __init__(self):
-        self.session_factory = SessionFactory().session_factory
-
     def __enter__(self) -> Session:
-        self.session = self.session_factory()
+        self.session = SessionFactory.get_session()
         return self.session
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
@@ -55,5 +61,5 @@ class SessionManager:
             raise e.with_traceback(exc_tb)
         finally:
             self.session.close()
-            self.session_factory.remove()
+            SessionFactory.remove_session()
         return False
