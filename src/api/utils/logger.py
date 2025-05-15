77f0d2dc5ef5147d426396e5d2aca55fe1540c10
Caio Martins <cmtabr@gmail.com>
2025-05-14 22:41:30 -0300

    chore: :bulb: simplifies logger class doc-string

--- src/api/utils/logger.py
+++ src/api/utils/logger.py
@@ -2,16 +2,7 @@ import logging
 
 
 class Logger:
-    """
-    Logger class to configure and provide a named logger instance.
-
-    Parameters:
-    -----------
-    logger_name : str
-        The name of the logger.
-    level : int, optional
-        Logging level (default is logging.INFO).
-    """
+    """Logger class for setting up a logger with a specific name and level"""
     def __init__(self, logger_name: str, level: int = logging.INFO):
         self.logger = logging.getLogger(name=logger_name)
         self.logger.setLevel(level)
